5ccef08154cc2f057ae7b8f2833b526ea622c7da
DAB0mB <emanor6@gmail.com>
2016-05-20 17:05:04 +0300

    Add api

---
+++ api/server/bootstrap.js
@@ -0,0 +1,27 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+Meteor.startup(function() {
+  if (Meteor.users.find().count() != 0) return;
+
+  Accounts.createUserWithPhone({
+    phone: '+972501234567',
+    profile: {
+      name: 'My friend 1'
+    }
+  });
+
+  Accounts.createUserWithPhone({
+    phone: '+972501234568',
+    profile: {
+      name: 'My friend 2'
+    }
+  });
+
+  Accounts.createUserWithPhone({
+    phone: '+972501234569',
+    profile: {
+      name: 'My friend 3'
+    }
+  });
+});
---
+++ api/server/methods.js
@@ -0,0 +1,84 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Chats, Messages } from './collections';
+
+Meteor.methods({
+  newMessage(message) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-logged-in',
+        'Must be logged in to send message.');
+    }
+
+    check(message, {
+      text: String,
+      chatId: String
+    });
+
+    message.timestamp = new Date();
+    message.userId = this.userId;
+
+    const messageId = Messages.insert(message);
+    Chats.update(message.chatId, { $set: { lastMessage: message } });
+
+    return messageId;
+  },
+
+  updateName(name) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-logged-in',
+        'Must be logged in to update his name.');
+    }
+
+    check(name, String);
+
+    if (name.length === 0) {
+      throw Meteor.Error('name-required', 'Must provide a user name');
+    }
+
+    return Meteor.users.update(this.userId, { $set: { 'profile.name': name } });
+  },
+
+  newChat(otherId) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-logged-in',
+        'Must be logged to create a chat.');
+    }
+
+    check(otherId, String);
+    const otherUser = Meteor.users.findOne(otherId);
+
+    if (!otherUser) {
+      throw new Meteor.Error('user-not-exists',
+        'Chat\'s user not exists');
+    }
+
+    const chat = {
+      userIds: [this.userId, otherId],
+      createdAt: new Date()
+    };
+
+    const chatId = Chats.insert(chat);
+
+    return chatId;
+  },
+
+  removeChat(chatId) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-logged-in',
+        'Must be logged to create a chat.');
+    }
+
+    check(chatId, String);
+
+    const chat = Chats.findOne(chatId);
+
+    if (!chat || !_.include(chat.userIds, this.userId)) {
+      throw new Meteor.Error('chat-not-exists',
+        'Chat not exists');
+    }
+
+    Messages.remove({ chatId: chatId });
+
+    return Chats.remove({ _id: chatId });
+  }
+});
