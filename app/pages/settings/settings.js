99aead7a73c6629380fe9ef71602becb1dd3afa6
DAB0mB <emanor6@gmail.com>
2016-06-12 01:11:35 +0300

    Implement settings view

--- app/pages/settings/settings.js
+++ app/pages/settings/settings.js
@@ -1,11 +1,46 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController, Alert} from 'ionic-angular';
+import {ProfilePage} from '../profile/profile';
+import {LoginPage} from '../login/login';
+import {UsersData} from '../../data-providers/users-data';
 
 
 @Page({
   templateUrl: 'build/pages/settings/settings.html'
 })
 export class SettingsPage {
-  constructor() {
+  static parameters = [[NavController], [UsersData]]
 
+  constructor(nav, users) {
+    this.nav = nav;
+    this.users = users;
+  }
+
+  editProfile() {
+    this.nav.push(ProfilePage);
+  }
+
+  logout() {
+    const alert = Alert.create({
+      title: 'Logout',
+      message: `Are you sure you would like to proceed?`,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            delete this.users.current;
+
+            this.nav.rootNav.setRoot(LoginPage, null, {
+              animate: true
+            });
+          }
+        }
+      ]
+    });
+
+    this.nav.present(alert);
   }
 }
