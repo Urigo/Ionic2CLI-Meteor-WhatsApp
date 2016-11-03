3c57e3ae8ed70fa6a705e1619521819c51632ddb
Dotan Simha <dotansimha@gmail.com>
2016-11-03 19:41:21 +0200

    Step 5.13: Add verification component

---
+++ src/pages/verification/verification.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+
+declare let Accounts;
+
+@Component({
+  selector: 'verification',
+  templateUrl: 'verification.html'
+})
+export class VerificationComponent implements OnInit {
+  code: string = '';
+  phone: string;
+
+  constructor(
+    private navCtrl: NavController,
+    private alertCtrl: AlertController,
+    private zone: NgZone,
+    private navParams: NavParams) {}
+
+  ngOnInit() {
+    this.phone = this.navParams.get('phone');
+  }
+
+  onInputKeypress({keyCode}: KeyboardEvent): void {
+    if (keyCode == 13) {
+      this.verify();
+    }
+  }
+
+  verify(): void {
+    Accounts.verifyPhone(this.phone, this.code, (e: Error) => {
+      this.zone.run(() => {
+        if (e) return this.handleError(e);
+
+        // this.navCtrl.setRoot(ProfileComponent, {}, {
+        //   animate: true
+        // });
+      });
+    });
+  }
+
+  private handleError(e: Error): void {
+    console.error(e);
+
+    const alert = this.alertCtrl.create({
+      title: 'Oops!',
+      message: e.message,
+      buttons: ['OK']
+    });
+
+    alert.present();
+  }
+}
