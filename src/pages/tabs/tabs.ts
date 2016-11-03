672810dc08ee8f60b61e069ac70cc8be8db9f720
Dotan Simha <dotansimha@gmail.com>
2016-11-03 15:03:11 +0200

    Step 2.8: Added chats tab root

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ChatsPage } from "../chats/chats";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+  chatsTab = ChatsPage;
+
   constructor() {
 
   }
