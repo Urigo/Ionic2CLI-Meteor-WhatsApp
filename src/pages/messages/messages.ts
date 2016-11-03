b38561c74247d1ae47337ab5c231eab3b9ec1dc1
Dotan Simha <dotansimha@gmail.com>
2016-11-03 18:46:46 +0200

    Step 4.6: Add basic messages component

--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from "@angular/core";
 import { NavParams } from "ionic-angular";
-import { Chat } from "api/models/whatsapp-models";
+import { Chat, Message } from "api/models/whatsapp-models";
+import { Messages } from "api/collections/whatsapp-collections";
+import { Observable} from "rxjs";
 
 @Component({
   selector: "messages-page",
-  template: `Messages Page`
+  templateUrl: "messages.html"
 })
 export class MessagesPage implements OnInit {
   private selectedChat: Chat;
+  private title: string;
+  private picture: string;
+  private messages: Observable<Message[]>;
 
   constructor(navParams: NavParams) {
     this.selectedChat = <Chat>navParams.get('chat');
-
-    console.log("Selected chat is: ", this.selectedChat);
+    this.title = this.selectedChat.title;
+    this.picture = this.selectedChat.picture;
   }
 
   ngOnInit() {
+    let isEven = false;
 
+    this.messages = Messages.find(
+      {chatId: this.selectedChat._id},
+      {sort: {createdAt: 1}}
+    ).map((messages: Message[]) => {
+      messages.forEach((message: Message) => {
+        message.ownership = isEven ? 'mine' : 'other';
+        isEven = !isEven;
+      });
+
+      return messages;
+    });
   }
 }
+
