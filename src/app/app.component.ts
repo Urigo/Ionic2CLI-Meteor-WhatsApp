1622c37613a682ae9cf7b9ae5bfd6325e2975835
Dotan Simha <dotansimha@gmail.com>
2016-11-03 19:39:12 +0200

    Step 5.13: Add user identifiation in app's main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { LoginComponent } from '../pages/auth/login';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  rootPage: any;
 
   constructor(platform: Platform) {
+    this.rootPage = Meteor.user() ? TabsPage : LoginComponent;
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
