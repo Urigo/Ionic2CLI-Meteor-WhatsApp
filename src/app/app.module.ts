7ddd4b5306a3196b6044662bbca70632c71f7b4d
Dotan Simha <dotansimha@gmail.com>
2016-11-03 19:38:55 +0200

    Step 5.12: Add login component to NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { ChatsPage } from "../pages/chats/chats";
 import { MomentModule } from "angular2-moment";
 import { MessagesPage } from "../pages/messages/messages";
+import { LoginComponent } from "../pages/auth/login";
 
 @NgModule({
   declarations: [
     MyApp,
     ChatsPage,
     TabsPage,
-    MessagesPage
+    MessagesPage,
+    LoginComponent
   ],
   imports: [
     IonicModule.forRoot(MyApp),
@@ -22,7 +24,8 @@ import { MessagesPage } from "../pages/messages/messages";
     MyApp,
     ChatsPage,
     TabsPage,
-    MessagesPage
+    MessagesPage,
+    LoginComponent
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
