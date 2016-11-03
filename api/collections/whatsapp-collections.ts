fccf9ad973a1afbd76a20dc795305eecbdd99488
Dotan Simha <dotansimha@gmail.com>
2016-11-03 20:22:58 +0200

    Step 6.5: Create Observable collection from Meteor.users

--- api/collections/whatsapp-collections.ts
+++ api/collections/whatsapp-collections.ts
@@ -1,4 +1,7 @@
 import { MongoObservable } from "meteor-rxjs";
 
+let Meteor = Package["meteor"].Meteor;
+
 export const Chats = new MongoObservable.Collection("chats");
 export const Messages = new MongoObservable.Collection("messages");
+export const Users = MongoObservable.fromExisting(Meteor.users);
