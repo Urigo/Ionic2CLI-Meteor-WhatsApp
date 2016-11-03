b09441e20c0e5ac0f123b1929030e3a16615c0d0
Dotan Simha <dotansimha@gmail.com>
2016-11-03 18:15:15 +0200

    Step 3.15: Added the chats with the last message using RxJS operators

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -1,66 +1,34 @@
-import * as moment from 'moment';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs";
 import { Chat } from "api/models/whatsapp-models";
+import { Chats, Messages } from "api/collections/whatsapp-collections";
 
 @Component({
   templateUrl: 'chats.html'
 })
-export class ChatsPage {
-  chats: Observable<Chat[]>;
+export class ChatsPage implements OnInit {
+  private chats;
 
   constructor() {
-    this.chats = this.findChats();
+
   }
 
-  private findChats(): Observable<Chat[]> {
-    return Observable.of([
-      {
-        _id: '0',
-        title: 'Ethan Gonzalez',
-        picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
-        lastMessage: {
-          content: 'You on your way?',
-          createdAt: moment().subtract(1, 'hours').toDate()
-        }
-      },
-      {
-        _id: '1',
-        title: 'Bryan Wallace',
-        picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
-        lastMessage: {
-          content: 'Hey, it\'s me',
-          createdAt: moment().subtract(2, 'hours').toDate()
-        }
-      },
-      {
-        _id: '2',
-        title: 'Avery Stewart',
-        picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
-        lastMessage: {
-          content: 'I should buy a boat',
-          createdAt: moment().subtract(1, 'days').toDate()
-        }
-      },
-      {
-        _id: '3',
-        title: 'Katie Peterson',
-        picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
-        lastMessage: {
-          content: 'Look at my mukluks!',
-          createdAt: moment().subtract(4, 'days').toDate()
-        }
-      },
-      {
-        _id: '4',
-        title: 'Ray Edwards',
-        picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
-        lastMessage: {
-          content: 'This is wicked good ice cream.',
-          createdAt: moment().subtract(2, 'weeks').toDate()
-        }
-      }
-    ]);
+  ngOnInit() {
+    this.chats = Chats
+      .find({})
+      .mergeMap((chats: Chat[]) =>
+        Observable.combineLatest(
+          ...chats.map((chat: Chat) =>
+            Messages
+              .find({chatId: chat._id})
+              .startWith(null)
+              .map(messages => {
+                if (messages) chat.lastMessage = messages[0];
+                return chat;
+              })
+          )
+        )
+      ).zone();
   }
 
   removeChat(chat: Chat): void {
