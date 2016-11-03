030c4ec07063f26307fc403f138db519e4075864
Dotan Simha <dotansimha@gmail.com>
2016-11-03 16:29:26 +0200

    Step 3.10: Created TypeScript typings file

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -11,6 +11,8 @@
   For more info on type definition files, check out the Typescript docs here:
   https://www.typescriptlang.org/docs/handbook/declaration-files/introduction.html
 */
+/// <reference types="meteor-typings" />
+/// <reference types="@types/underscore" />
 /// <reference path="../models/whatsapp-models.d.ts" />
 declare module '*';
 
