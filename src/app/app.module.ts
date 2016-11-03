a30b30d676b4a1bfc6abd2ef6d9a0d5e1a60704e
Dotan Simha <dotansimha@gmail.com>
2016-11-03 15:02:47 +0200

    Step 2.7: Added ChatsPage to the module definition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ChatsPage } from "../pages/chats/chats";
 
 @NgModule({
   declarations: [
     MyApp,
+    ChatsPage,
     TabsPage
   ],
   imports: [
@@ -14,6 +16,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    ChatsPage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
