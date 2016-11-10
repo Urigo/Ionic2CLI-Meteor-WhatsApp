28a29a18a0ed3b2a034bc8809e0570b0bc286b2b
DAB0mB <emanor6@gmail.com>
2016-11-10 13:49:11 -0200

    Step 3.13: Updated import path

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -11,5 +11,5 @@
   For more info on type definition files, check out the Typescript docs here:
   https://www.typescriptlang.org/docs/handbook/declaration-files/introduction.html
 */
-/// <reference path="../models/whatsapp-models.d.ts" />
+/// <reference path="../api/models/whatsapp-models.d.ts" />
 declare module '*';
