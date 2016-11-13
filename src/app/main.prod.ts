9934af2710b696a8ad90750479547be975e1afde
DAB0mB <emanor6@gmail.com>
2016-11-13 14:15:45 -0200

    Step 5.9: Wait for user if logging in, production

--- src/app/main.prod.ts
+++ src/app/main.prod.ts
@@ -5,8 +5,21 @@ import 'accounts-phone';
 
 import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
-
 import { AppModuleNgFactory } from './app.module.ngfactory';
+import { MeteorObservable } from 'meteor-rxjs';
 
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+declare let Meteor;
+
+Meteor.startup(() => {
+  const sub = MeteorObservable.autorun().subscribe(() => {
+    if (Meteor.loggingIn()) return;
+
+    setTimeout(() => {
+      sub.unsubscribe();
+    });
+
+    enableProdMode();
+    platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+  });
+});
+
