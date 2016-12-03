49c54dc53cb42df589d7c45e52dab03d95b0a60d
DAB0mB <emanor6@gmail.com>
2016-12-03 00:01:57 -0200

    Step 7.14: Add meteor-publish-composite type declarations

--- api/typings.d.ts
+++ api/typings.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="@types/meteor" />
 /// <reference types="@types/meteor-accounts-phone" />
+/// <reference types="@types/meteor-publish-composite" />
 /// <reference types="@types/underscore" />
 /// <reference types="moment" />
