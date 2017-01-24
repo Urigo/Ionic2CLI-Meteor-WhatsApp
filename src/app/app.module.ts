8a7cb3012f3aa15bd83be21f44c974cabed47543
dotansimha <dotansimha@gmail.com>
2017-01-24 14:29:28 +0200

    Step 6.4: Import MessagesPage in the NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { MomentModule } from 'angular2-moment';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { ChatsPage } from '../pages/chats/chats';
+import { MessagesPage } from '../pages/messages/messages';
 import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [
     MyApp,
-    ChatsPage
+    ChatsPage,
+    MessagesPage
   ],
   imports: [
     IonicModule.forRoot(MyApp),
@@ -16,7 +18,8 @@ import { MyApp } from './app.component';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    ChatsPage
+    ChatsPage,
+    MessagesPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
