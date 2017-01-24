5b491fcd3a19bf34a6c2a886e8cc1797cc81f411
dotansimha <dotansimha@gmail.com>
2017-01-25 00:15:44 +0200

    Step 13.24: Add pictureId property to Profile

--- api/server/models.ts
+++ api/server/models.ts
@@ -3,6 +3,7 @@ export const DEFAULT_PICTURE_URL = '/assets/default-profile-pic.svg';
 export interface Profile {
   name?: string;
   picture?: string;
+  pictureId?: string;
 }
 
 export enum MessageType {
