6a8576d9b9e2feda0183c7a8a76ece58b9da0760
dotansimha <dotansimha@gmail.com>
2017-01-24 19:06:35 +0200

    Step 9.2: Add removeChat method on server side

--- api/server/methods.ts
+++ api/server/methods.ts
@@ -37,6 +37,23 @@ Meteor.methods({
 
     Chats.insert(chat);
   },
+  removeChat(chatId: string): void {
+    if (!this.userId) {
+      throw new Meteor.Error('unauthorized',
+        'User must be logged-in to remove chat');
+    }
+
+    check(chatId, nonEmptyString);
+
+    const chatExists = !!Chats.collection.find(chatId).count();
+
+    if (!chatExists) {
+      throw new Meteor.Error('chat-not-exists',
+        'Chat doesn\'t exist');
+    }
+
+    Chats.remove(chatId);
+  },
   updateProfile(profile: Profile): void {
     if (!this.userId) throw new Meteor.Error('unauthorized',
       'User must be logged-in to create a new chat');
