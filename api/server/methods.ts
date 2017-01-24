682653ef2722d6c3a17f320d58ae2531d738ff30
dotansimha <dotansimha@gmail.com>
2017-01-24 15:14:19 +0200

    Step 6.17: Use check to add validations

--- api/server/methods.ts
+++ api/server/methods.ts
@@ -1,9 +1,19 @@
 import { Chats } from './collections/chats';
 import { Messages } from './collections/messages';
 import { MessageType } from './models';
+import { check, Match } from 'meteor/check';
+
+const nonEmptyString = Match.Where((str) => {
+  check(str, String);
+  return str.length > 0;
+});
 
 Meteor.methods({
   addMessage(type: MessageType, chatId: string, content: string) {
+    check(type, Match.OneOf(String, [ MessageType.TEXT ]));
+    check(chatId, nonEmptyString);
+    check(content, nonEmptyString);
+
     const chatExists = !!Chats.collection.find(chatId).count();
 
     if (!chatExists) {
