c5f43e240721c6e544e4aea549f0036da63ce663
dotansimha <dotansimha@gmail.com>
2017-01-24 09:56:28 +0200

    Step 2.7: Add stubs for chats objects

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   templateUrl: 'chats.html'
 })
 export class ChatsPage {
-  constructor() {
+  chats: Observable<any[]>;
 
+  constructor() {
+    this.chats = this.findChats();
+  }
+
+  private findChats(): Observable<any[]> {
+    return Observable.of([
+      {
+        _id: '0',
+        title: 'Ethan Gonzalez',
+        picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
+        lastMessage: {
+          content: 'You on your way?',
+          createdAt: moment().subtract(1, 'hours').toDate()
+        }
+      },
+      {
+        _id: '1',
+        title: 'Bryan Wallace',
+        picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
+        lastMessage: {
+          content: 'Hey, it\'s me',
+          createdAt: moment().subtract(2, 'hours').toDate()
+        }
+      },
+      {
+        _id: '2',
+        title: 'Avery Stewart',
+        picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
+        lastMessage: {
+          content: 'I should buy a boat',
+          createdAt: moment().subtract(1, 'days').toDate()
+        }
+      },
+      {
+        _id: '3',
+        title: 'Katie Peterson',
+        picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
+        lastMessage: {
+          content: 'Look at my mukluks!',
+          createdAt: moment().subtract(4, 'days').toDate()
+        }
+      },
+      {
+        _id: '4',
+        title: 'Ray Edwards',
+        picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
+        lastMessage: {
+          content: 'This is wicked good ice cream.',
+          createdAt: moment().subtract(2, 'weeks').toDate()
+        }
+      }
+    ]);
   }
 }
