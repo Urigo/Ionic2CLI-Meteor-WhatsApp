cbd06c13de2bed01ff3618185864654d6f3e40b3
dotansimha <dotansimha@gmail.com>
2017-01-24 19:21:34 +0200

    Step 9.11: Add users publication

---
+++ api/server/publications.ts
@@ -0,0 +1,14 @@
+import { User } from './models';
+import { Users } from './collections/users';
+
+Meteor.publish('users', function(): Mongo.Cursor<User> {
+  if (!this.userId) {
+    return;
+  }
+
+  return Users.collection.find({}, {
+    fields: {
+      profile: 1
+    }
+  });
+});
