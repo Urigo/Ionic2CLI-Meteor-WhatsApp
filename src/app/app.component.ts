ef565d657d32c79259e394e046a7d0b375a3a2d7
dotansimha <dotansimha@gmail.com>
2017-01-24 09:53:44 +0200

    Step 2.6: Use the chats page as the main root page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
-import { TabsPage } from '../pages/tabs/tabs';
-
+import { ChatsPage } from '../pages/chats/chats';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  rootPage = ChatsPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
