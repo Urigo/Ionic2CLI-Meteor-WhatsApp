7c643a2b83e993e7d2d6f4a9ffdb48b9798c522f
dotansimha <dotansimha@gmail.com>
2017-01-24 10:05:29 +0200

    Step 2.10: Use TypeScript models

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import * as moment from 'moment';
+import { Chat, MessageType } from '../../models';
 
 @Component({
   templateUrl: 'chats.html'
 })
 export class ChatsPage {
-  chats: Observable<any[]>;
+  chats: Observable<Chat[]>;
 
   constructor() {
     this.chats = this.findChats();
   }
 
-  private findChats(): Observable<any[]> {
+  private findChats(): Observable<Chat[]> {
     return Observable.of([
       {
         _id: '0',
@@ -20,7 +21,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
         lastMessage: {
           content: 'You on your way?',
-          createdAt: moment().subtract(1, 'hours').toDate()
+          createdAt: moment().subtract(1, 'hours').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -29,7 +31,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
         lastMessage: {
           content: 'Hey, it\'s me',
-          createdAt: moment().subtract(2, 'hours').toDate()
+          createdAt: moment().subtract(2, 'hours').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -38,7 +41,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
         lastMessage: {
           content: 'I should buy a boat',
-          createdAt: moment().subtract(1, 'days').toDate()
+          createdAt: moment().subtract(1, 'days').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -47,7 +51,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
         lastMessage: {
           content: 'Look at my mukluks!',
-          createdAt: moment().subtract(4, 'days').toDate()
+          createdAt: moment().subtract(4, 'days').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -56,7 +61,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
         lastMessage: {
           content: 'This is wicked good ice cream.',
-          createdAt: moment().subtract(2, 'weeks').toDate()
+          createdAt: moment().subtract(2, 'weeks').toDate(),
+          type: MessageType.TEXT
         }
       }
     ]);
