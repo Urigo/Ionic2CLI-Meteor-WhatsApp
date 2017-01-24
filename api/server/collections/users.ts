9c47bdb786c7ed72a89964f83aad87edb439fe57
dotansimha <dotansimha@gmail.com>
2017-01-25 00:19:05 +0200

    Step 13.26: Add after hook for user modification

--- api/server/collections/users.ts
+++ api/server/collections/users.ts
@@ -1,5 +1,15 @@
 import { MongoObservable } from 'meteor-rxjs';
 import { Meteor } from 'meteor/meteor';
 import { User } from '../models';
+import { Pictures } from './pictures';
 
 export const Users = MongoObservable.fromExisting<User>(Meteor.users);
+
+// Dispose unused profile pictures
+Meteor.users.after.update(function (userId, doc, fieldNames, modifier, options) {
+  if (!doc.profile) return;
+  if (!this.previous.profile) return;
+  if (doc.profile.pictureId == this.previous.profile.pictureId) return;
+
+  Pictures.collection.remove({ _id: this.previous.profile.pictureId });
+}, { fetchPrevious: true });
