0ba2a5588eac76ec7ca235d88d1ec11440d3bca8
dotansimha <dotansimha@gmail.com>
2017-01-24 22:32:43 +0200

    Step 12.6: Implement sendPicture method

--- src/pages/messages/messages-attachments.ts
+++ src/pages/messages/messages-attachments.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AlertController, Platform, ModalController, ViewController } from 'ionic-angular';
 import { NewLocationMessageComponent } from './location-message';
 import { MessageType } from 'api/models';
+import { PictureService } from '../../services/picture';
 
 @Component({
   selector: 'messages-attachments',
@@ -12,8 +13,18 @@ export class MessagesAttachmentsComponent {
     private alertCtrl: AlertController,
     private platform: Platform,
     private viewCtrl: ViewController,
-    private modelCtrl: ModalController
+    private modelCtrl: ModalController,
+    private pictureService: PictureService
   ) {}
+
+  sendPicture(): void {
+    this.pictureService.select().then((file: File) => {
+      this.viewCtrl.dismiss({
+        messageType: MessageType.PICTURE,
+        selectedPicture: file
+      });
+    });
+  }
 
   sendLocation(): void {
     const locationModal = this.modelCtrl.create(NewLocationMessageComponent);
