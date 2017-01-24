06d8db00ccd4b928e276ebc687549120cfeff459
dotansimha <dotansimha@gmail.com>
2017-01-24 14:38:44 +0200

    Step 6.6: Add basic messages component

--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -1,21 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams } from 'ionic-angular';
-import { Chat } from 'api/models';
+import { Chat, Message } from 'api/models';
+import { Observable } from 'rxjs';
+import { Messages } from 'api/collections';
 
 @Component({
   selector: 'messages-page',
-  template: `Messages Page`
+  templateUrl: 'messages.html'
 })
 export class MessagesPage implements OnInit {
   selectedChat: Chat;
+  title: string;
+  picture: string;
+  messages: Observable<Message[]>;
 
   constructor(navParams: NavParams) {
     this.selectedChat = <Chat>navParams.get('chat');
-
-    console.log('Selected chat is: ', this.selectedChat);
+    this.title = this.selectedChat.title;
+    this.picture = this.selectedChat.picture;
   }
 
   ngOnInit() {
+    let isEven = false;
 
+    this.messages = Messages.find(
+      {chatId: this.selectedChat._id},
+      {sort: {createdAt: 1}}
+    ).map((messages: Message[]) => {
+      messages.forEach((message: Message) => {
+        message.ownership = isEven ? 'mine' : 'other';
+        isEven = !isEven;
+      });
+
+      return messages;
+    });
   }
 }
