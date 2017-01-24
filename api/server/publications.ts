d20ea22cdaac22d6177a92d98b3cdc07c21f6afe
dotansimha <dotansimha@gmail.com>
2017-01-24 19:22:48 +0200

    Step 9.12: Publish messages

--- api/server/publications.ts
+++ api/server/publications.ts
@@ -1,5 +1,6 @@
-import { User } from './models';
+import { User, Message } from './models';
 import { Users } from './collections/users';
+import { Messages } from './collections/messages';
 
 Meteor.publish('users', function(): Mongo.Cursor<User> {
   if (!this.userId) {
@@ -10,5 +11,17 @@ Meteor.publish('users', function(): Mongo.Cursor<User> {
     fields: {
       profile: 1
     }
+  });
+});
+
+Meteor.publish('messages', function(chatId: string): Mongo.Cursor<Message> {
+  if (!this.userId || !chatId) {
+    return;
+  }
+
+  return Messages.collection.find({
+    chatId
+  }, {
+    sort: { createdAt: -1 }
   });
 });
