ecc4b48f4b480de859234815fccbf05930f41207
dotansimha <dotansimha@gmail.com>
2017-01-24 17:51:45 +0200

    Step 8.4: Added new chat component

---
+++ src/pages/chats/new-chat.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { Chats, Users } from 'api/collections';
+import { User } from 'api/models';
+import { AlertController, ViewController } from 'ionic-angular';
+import { MeteorObservable } from 'meteor-rxjs';
+import * as _ from 'lodash';
+import { Observable, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'new-chat',
+  templateUrl: 'new-chat.html'
+})
+export class NewChatComponent implements OnInit {
+  senderId: string;
+  users: Observable<User[]>;
+  usersSubscription: Subscription;
+
+  constructor(
+    private alertCtrl: AlertController,
+    private viewCtrl: ViewController
+  ) {
+    this.senderId = Meteor.userId();
+  }
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  addChat(user): void {
+    MeteorObservable.call('addChat', user._id).subscribe({
+      next: () => {
+        this.viewCtrl.dismiss();
+      },
+      error: (e: Error) => {
+        this.viewCtrl.dismiss().then(() => {
+          this.handleError(e);
+        });
+      }
+    });
+  }
+
+  loadUsers(): void {
+    this.users = this.findUsers();
+  }
+
+  findUsers(): Observable<User[]> {
+    // Find all belonging chats
+    return Chats.find({
+      memberIds: this.senderId
+    }, {
+      fields: {
+        memberIds: 1
+      }
+    })
+    // Invoke merge-map with an empty array in case no chat found
+    .startWith([])
+    .mergeMap((chats) => {
+      // Get all userIDs who we're chatting with
+      const receiverIds = _.chain(chats)
+        .map('memberIds')
+        .flatten()
+        .concat(this.senderId)
+        .value();
+
+      // Find all users which are not in belonging chats
+      return Users.find({
+        _id: { $nin: receiverIds }
+      })
+      // Invoke map with an empty array in case no user found
+      .startWith([]);
+    });
+  }
+
+  handleError(e: Error): void {
+    console.error(e);
+
+    const alert = this.alertCtrl.create({
+      buttons: ['OK'],
+      message: e.message,
+      title: 'Oops!'
+    });
+
+    alert.present();
+  }
+}
