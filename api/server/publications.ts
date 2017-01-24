4d520cb4095cd0facd8c7336d4c14ef3eaf9b6c6
dotansimha <dotansimha@gmail.com>
2017-01-24 19:28:32 +0200

    Step 9.16: Implement chats publication

--- api/server/publications.ts
+++ api/server/publications.ts
@@ -1,6 +1,7 @@
-import { User, Message } from './models';
+import { User, Message, Chat } from './models';
 import { Users } from './collections/users';
 import { Messages } from './collections/messages';
+import { Chats } from './collections/chats';
 
 Meteor.publish('users', function(): Mongo.Cursor<User> {
   if (!this.userId) {
@@ -24,4 +25,36 @@ Meteor.publish('messages', function(chatId: string): Mongo.Cursor<Message> {
   }, {
     sort: { createdAt: -1 }
   });
+});
+
+Meteor.publishComposite('chats', function(): PublishCompositeConfig<Chat> {
+  if (!this.userId) {
+    return;
+  }
+
+  return {
+    find: () => {
+      return Chats.collection.find({ memberIds: this.userId });
+    },
+
+    children: [
+      <PublishCompositeConfig1<Chat, Message>> {
+        find: (chat) => {
+          return Messages.collection.find({ chatId: chat._id }, {
+            sort: { createdAt: -1 },
+            limit: 1
+          });
+        }
+      },
+      <PublishCompositeConfig1<Chat, User>> {
+        find: (chat) => {
+          return Users.collection.find({
+            _id: { $in: chat.memberIds }
+          }, {
+            fields: { profile: 1 }
+          });
+        }
+      }
+    ]
+  };
 });
