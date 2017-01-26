ec1ea7d967b1c47845dad337fcba33cbe51fbcaa
dotansimha <dotansimha@gmail.com>
2017-01-26 17:00:24 +0200

    Step 7.28: Added updateProfile method

--- api/server/methods.ts
+++ api/server/methods.ts
@@ -1,6 +1,6 @@
 import { Chats } from './collections/chats';
 import { Messages } from './collections/messages';
-import { MessageType } from './models';
+import { MessageType, Profile } from './models';
 import { check, Match } from 'meteor/check';
 
 const nonEmptyString = Match.Where((str) => {
@@ -9,6 +9,18 @@ const nonEmptyString = Match.Where((str) => {
 });
 
 Meteor.methods({
+  updateProfile(profile: Profile): void {
+    if (!this.userId) throw new Meteor.Error('unauthorized',
+      'User must be logged-in to create a new chat');
+
+    check(profile, {
+      name: nonEmptyString
+    });
+
+    Meteor.users.update(this.userId, {
+      $set: {profile}
+    });
+  },
   addMessage(type: MessageType, chatId: string, content: string) {
     check(type, Match.OneOf(String, [ MessageType.TEXT ]));
     check(chatId, nonEmptyString);
