b01640393e73f8491fe4d5fd83d0b2aec64629aa
DAB0mB <emanor6@gmail.com>
2017-01-26 14:55:37 -0200

    Step 1.10: Check if cordova exists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,10 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      if (platform.is('cordova')) {
+        statusBar.styleDefault();
+        splashScreen.hide();
+      }
     });
   }
 }
