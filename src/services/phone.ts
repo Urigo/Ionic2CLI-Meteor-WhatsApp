571c664d936d3db3e1162884edd1f1b673391d22
DAB0mB <emanor6@gmail.com>
2017-01-27 03:19:14 -0200

    Step 7.8: Added phone service

---
+++ src/services/phone.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Accounts } from 'meteor/accounts-base';
+import { Meteor } from 'meteor/meteor';
+
+@Injectable()
+export class PhoneService {
+  verify(phoneNumber: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      Accounts.requestPhoneVerification(phoneNumber, (e: Error) => {
+        if (e) {
+          return reject(e);
+        }
+
+        resolve();
+      });
+    });
+  }
+
+  login(phoneNumber: string, code: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      Accounts.verifyPhone(phoneNumber, code, (e: Error) => {
+        if (e) {
+          return reject(e);
+        }
+
+        resolve();
+      });
+    });
+  }
+
+  logout(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      Meteor.logout((e: Error) => {
+        if (e) {
+          return reject(e);
+        }
+
+        resolve();
+      });
+    });
+  }
+}
