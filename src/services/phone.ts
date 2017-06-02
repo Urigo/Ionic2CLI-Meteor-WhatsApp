fff952425d3d69935425222b3a02cb530e78be7c
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-06-02 18:38:31 +0200

    Step 14.4: Implement getNumber with native ionic

--- src/services/phone.ts
+++ src/services/phone.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
+import { Platform } from 'ionic-angular';
+import { Sim } from '@ionic-native/sim';
 
 @Injectable()
 export class PhoneService {
+  constructor(private platform: Platform,
+              private sim: Sim) {
+
+  }
+
+  async getNumber(): Promise<string> {
+    if (!this.platform.is('cordova')) {
+      throw new Error('Cannot read SIM, platform is not Cordova.')
+    }
+
+    if (!(await this.sim.hasReadPermission())) {
+      try {
+        await this.sim.requestReadPermission();
+      } catch (e) {
+        throw new Error('User denied SIM access.');
+      }
+    }
+
+    return '+' + (await this.sim.getSimInfo()).phoneNumber;
+  }
+
   verify(phoneNumber: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
       Accounts.requestPhoneVerification(phoneNumber, (e: Error) => {
