fa0c05f9c9933dbf0bb004c588e5007646a1a6ee
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-06-03 16:19:07 +0200

    Step 14.11: Add getSMS method to phone.ts

--- src/services/phone.ts
+++ src/services/phone.ts
@@ -3,12 +3,17 @@ import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 import { Platform } from 'ionic-angular';
 import { Sim } from '@ionic-native/sim';
+import { SmsReceiver } from "../ionic/sms-receiver";
+import * as Bluebird from "bluebird";
+import { TWILIO_SMS_NUMBERS } from "api/models";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class PhoneService {
   constructor(private platform: Platform,
-              private sim: Sim) {
-
+              private sim: Sim,
+              private smsReceiver: SmsReceiver) {
+    Bluebird.promisifyAll(this.smsReceiver);
   }
 
   async getNumber(): Promise<string> {
@@ -25,6 +30,38 @@ export class PhoneService {
     }
 
     return '+' + (await this.sim.getSimInfo()).phoneNumber;
+  }
+
+  async getSMS(): Promise<string> {
+    if (!this.platform.is('android')) {
+      throw new Error('Cannot read SMS, platform is not Android.')
+    }
+
+    try {
+      await (<any>this.smsReceiver).isSupported();
+    } catch (e) {
+      throw new Error('User denied SMS access.');
+    }
+
+    const startObs = Observable.fromPromise((<any>this.smsReceiver).startReceiving()).map((msg: string) => msg);
+    const timeoutObs = Observable.interval(120000).take(1).map(() => {
+      throw new Error('Receiving SMS timed out.')
+    });
+
+    try {
+      var msg = await startObs.takeUntil(timeoutObs).toPromise();
+    } catch (e) {
+      await (<any>this.smsReceiver).stopReceiving();
+      throw e;
+    }
+
+    await (<any>this.smsReceiver).stopReceiving();
+
+    if (TWILIO_SMS_NUMBERS.includes(msg.split(">")[0])) {
+      return msg.substr(msg.length - 4);
+    } else {
+      throw new Error('Sender is not a Twilio number.')
+    }
   }
 
   verify(phoneNumber: string): Promise<void> {
