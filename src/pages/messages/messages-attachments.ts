f7cd71f3c92fd91611f2cd9e814b30000100d4a3
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-06-07 12:14:15 +0200

    Step 14.15: Use the getPicture method into messages-attachment.ts

--- src/pages/messages/messages-attachments.ts
+++ src/pages/messages/messages-attachments.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ModalController, ViewController } from 'ionic-angular';
+import { AlertController, ModalController, Platform, ViewController } from 'ionic-angular';
 import { NewLocationMessageComponent } from './location-message';
 import { MessageType } from 'api/models';
 import { PictureService } from '../../services/picture';
@@ -12,16 +12,26 @@ export class MessagesAttachmentsComponent {
   constructor(
     private viewCtrl: ViewController,
     private modelCtrl: ModalController,
-    private pictureService: PictureService
+    private pictureService: PictureService,
+    private platform: Platform,
+    private alertCtrl: AlertController
   ) {}
 
-  sendPicture(): void {
-    this.pictureService.select().then((file: File) => {
-      this.viewCtrl.dismiss({
-        messageType: MessageType.PICTURE,
-        selectedPicture: file
+  sendPicture(camera: boolean): void {
+    if (camera && !this.platform.is('cordova')) {
+      return console.warn('Device must run cordova in order to take pictures');
+    }
+
+    this.pictureService.getPicture(camera, false)
+      .then((blob: File) => {
+        this.viewCtrl.dismiss({
+          messageType: MessageType.PICTURE,
+          selectedPicture: blob
+        });
+      })
+      .catch((e) => {
+        this.handleError(e);
       });
-    });
   }
 
   sendLocation(): void {
@@ -40,5 +50,17 @@ export class MessagesAttachmentsComponent {
     });
 
     locationModal.present();
+  }
+
+  handleError(e: Error): void {
+    console.error(e);
+
+    const alert = this.alertCtrl.create({
+      title: 'Oops!',
+      message: e.message,
+      buttons: ['OK']
+    });
+
+    alert.present();
   }
 }
