2ff4f0e0c1371a90e0200a743f5d76eb4bb25eef
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-06-09 17:24:51 +0200

    Step 17.3: Configure oauth services using service-configuration

--- api/server/main.ts
+++ api/server/main.ts
@@ -1,9 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
+declare const ServiceConfiguration: any;
 
 Meteor.startup(() => {
   if (Meteor.settings) {
     Object.assign(Accounts._options, Meteor.settings['accounts-phone']);
     SMS.twilio = Meteor.settings['twilio'];
+  }
+
+  // Configuring oAuth services
+  const services = Meteor.settings.private.oAuth;
+
+  if (services) {
+    for (let service in services) {
+      ServiceConfiguration.configurations.upsert({service: service}, {
+        $set: services[service]
+      });
+    }
   }
 });
